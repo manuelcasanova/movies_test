d53f71cfa5025baf77e37362ea79cf386e8061cd
Manuel Casanova <manucasanova@hotmail.com>
2022-07-12 17:01:27 -0700

    Install nodemon

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8001;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json()); //req.body
+
+app.listen(port, () => {
+  console.log(`Movies app running on port ${port}.`);
+});
